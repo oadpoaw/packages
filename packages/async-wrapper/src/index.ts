cbecff9ae541dda0634523d50589452dbbee9b0f
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-07 15:05:17 +0800

    chore: prettier format

--- packages/async-wrapper/src/index.ts
+++ packages/async-wrapper/src/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
-export default function AsyncWrapper(fn: (req: Request, res: Response, next: NextFunction) => any) {
+export default function AsyncWrapper(
+    fn: (req: Request, res: Response, next: NextFunction) => any,
+) {
     return function (req: Request, res: Response, next: NextFunction) {
         fn(req, res, next).catch((err: any) => {
             next(new HTTPError(err, req));
@@ -8,9 +10,15 @@ export default function AsyncWrapper(fn: (req: Request, res: Response, next: Nex
     };
 }
 
-export function makeCatcher(Logger: { error: (err: any) => any; } | false = false) {
-    return function (err: HTTPError, _req: Request, res: Response, _next: NextFunction) {
-
+export function makeCatcher(
+    Logger: { error: (err: any) => any } | false = false,
+) {
+    return function (
+        err: HTTPError,
+        _req: Request,
+        res: Response,
+        _next: NextFunction,
+    ) {
         if (Logger) Logger.error(err.error);
 
         res.status(err.status ?? 404);
@@ -18,15 +26,18 @@ export function makeCatcher(Logger: { error: (err: any) => any; } | false = fals
         if (!res.headersSent) {
             res.json({
                 message: err.message ?? 'Not Found',
-                error: process.env.NODE_ENV === 'production' ? (!err ? {} : err) : null,
+                error:
+                    process.env.NODE_ENV === 'production'
+                        ? !err
+                            ? {}
+                            : err
+                        : null,
             });
         }
-
     };
 }
 
 export class HTTPError extends Error {
-
     constructor(err: Error, req: Request, status = 500) {
         super(err.message);
         this.name = err.name;
@@ -40,5 +51,4 @@ export class HTTPError extends Error {
     method: string;
     path: string;
     error!: Error;
-
 }
