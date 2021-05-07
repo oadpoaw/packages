df7b0dd58b46db4358111167d43f587c8b06578c
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-07 12:34:46 +0800

    chore: format tests

--- packages/array-chunk/src/index.ts
+++ packages/array-chunk/src/index.ts
@@ -1,13 +1,12 @@
 /**
- * 
+ *
  * @param array The Array to chunkify
  * @param size Size of the chunks
  */
- export default function ArrayChunk<T>(array: T[], size: number): T[][] {
+export default function ArrayChunk<T>(array: T[], size: number): T[][] {
     const clone = array.slice();
 
     return new Array(Math.ceil(clone.length / size))
         .fill(0)
-        .map(_ => clone.splice(0, size));
-  
+        .map((_) => clone.splice(0, size));
 }
