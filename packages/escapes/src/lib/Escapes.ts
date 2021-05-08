3f311da38b150f859409ed8eff435a79a63c2a0c
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-08 21:38:23 +0800

    chore: escapes

--- packages/escapes/src/lib/Escapes.ts
+++ packages/escapes/src/lib/Escapes.ts
@@ -1,41 +1,33 @@
-export class Escapes {
-    constructor() {
-        throw new Error(
-            `The '${this.constructor.name}' class cannot be instantiated.`,
-        );
-    }
-
+export const Escapes = {
     /**
      * Escapes Special Characters to be compatible with Regexs
      * So special characters would not error out regex parsing
      */
-    static regex(str: string) {
+    regex: (str: string) => {
         if (typeof str !== 'string')
             throw new TypeError(
                 `Escapes.regex(str): 'str' should be a string.`,
             );
         return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    }
-
+    },
     /**
      * Escapes the character `@` address sign
      */
-    static addressSign(str: string) {
+    addressSign: (str: string) => {
         if (typeof str !== 'string')
             throw new TypeError(
                 `Escapes.regex(str): 'str' should be a string.`,
             );
         return str.replace(/@/g, `@${String.fromCharCode(8203)}`);
-    }
-
+    },
     /**
      * Escapes the character ` backtick
      */
-    static backticks(str: string) {
+    backticks: (str: string) => {
         if (typeof str !== 'string')
             throw new TypeError(
                 `Escapes.regex(str): 'str' should be a string.`,
             );
         return str.replace(/`/g, `\`${String.fromCharCode(8203)}`);
-    }
-}
+    },
+};
