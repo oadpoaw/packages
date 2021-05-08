0f0aede0b58e2260fa554ecf73a8eed999040e5c
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-08 13:21:48 +0800

    test: errors

--- packages/shorten/__tests__/index.test.ts
+++ packages/shorten/__tests__/index.test.ts
@@ -4,13 +4,13 @@ test('shorten', () => {
     expect(shorten('1234567890', 10)).toStrictEqual('1234567890');
     expect(shorten('12345678900', 10)).toStrictEqual('1234567...');
 
-    //@ts-expect-error
     expect(() => {
+        //@ts-expect-error
         shorten(123, '31221');
     }).toThrow();
 
-    //@ts-expect-error
     expect(() => {
+        //@ts-expect-error
         shorten('123123', '12213');
     }).toThrow();
 });
