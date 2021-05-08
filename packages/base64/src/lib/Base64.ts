5339fff55bb5fabb777abb96799a77b1a93f06d1
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-08 21:35:57 +0800

    chore: base64

--- packages/base64/src/lib/Base64.ts
+++ packages/base64/src/lib/Base64.ts
@@ -1,23 +1,16 @@
-export class Base64 {
-    constructor() {
-        throw new Error(
-            `The '${this.constructor.name}' class cannot be instantiated.`,
-        );
-    }
-
-    static encode(str: string) {
+export const Base64 = {
+    encode: (str: string) => {
         if (typeof str !== 'string')
             throw new TypeError(
                 `Base64.encode(str): 'str' should be a string.`,
             );
         return Buffer.from(str).toString('base64');
-    }
-
-    static decode(str: string) {
+    },
+    decode: (str: string) => {
         if (typeof str !== 'string')
             throw new TypeError(
                 `Base64.decode(str): 'str' should be a string.`,
             );
         return Buffer.from(str, 'base64').toString('utf-8');
-    }
-}
+    },
+};
