e7a068d7e2d2bf19f1a2314dce336df882e25c4b
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-08 13:29:05 +0800

    fix: add type checking

--- packages/wait/src/lib/wait.ts
+++ packages/wait/src/lib/wait.ts
@@ -1,3 +1,5 @@
 export function wait(ms: number) {
+    if (typeof ms !== 'number')
+        throw new TypeError(`wait(ms): 'ms' should be a number.`);
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
