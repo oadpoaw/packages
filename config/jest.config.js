780b4d59f3febf7846b5811bc92fcc4645d9384a
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-08 21:10:30 +0800

    chore: docgen

---
+++ config/jest.config.js
@@ -0,0 +1,36 @@
+//@ts-check
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('../tsconfig.json');
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+module.exports = {
+    testMatch: [
+        '**/+(*.)+(spec|test).+(ts|js)?(x)'
+    ],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageProvider: 'v8',
+    coverageDirectory: '../coverage',
+    coverageReporters: [
+        'text',
+        'lcov',
+        'clover'
+    ],
+    coverageThreshold: {
+        global: {
+            'branches': 80,
+            'functions': 80,
+            'lines': 80,
+            'statements': 80
+        }
+    },
+    roots: [
+        '<rootDir>/../packages/'
+    ],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/../'
+    })
+};
