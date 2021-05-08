fa4541cfc4307462754e0e6133bac1572dba8056
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-08 10:30:24 +0800

    chore(asyncWrapper): import as type

--- packages/async-wrapper/src/index.ts
+++ packages/async-wrapper/src/index.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 export default function AsyncWrapper(
     fn: (req: Request, res: Response, next: NextFunction) => any,
